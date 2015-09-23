3d1c80e402d72cae00de7180f52bbee7fc59281f
unknown <Sofyan_A@MTPC7043.mitrais.com>
2015-09-23 16:26:21 +0700

    Start fix dashboard icon

--- application/views/home.php
+++ application/views/home.php
@@ -2,8 +2,6 @@
 	$this->load->view("partial/header"); 
 ?>
 
-<br />
-<h3><?php echo $this->lang->line('common_welcome_message'); ?></h3>
 <div id="home_module_list">
 	<?php
 	foreach($allowed_modules->result() as $module)
@@ -12,13 +10,15 @@
 		{
 	?>
 	<div class="module-item col-md-6" >
-		<a href="<?php echo site_url("$module->module_id");?>">		
-			<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" class="circle" />
-			<br />				
-			<?php echo $this->lang->line("module_".$module->module_id) ?>
-		</a>
-		
-		 - <?php echo $this->lang->line('module_'.$module->module_id.'_desc');?>
+		<div id="module-icon-<?php echo $module->module_id;?>">
+			<a href="<?php echo site_url("$module->module_id");?>" >		
+				<img src="<?php echo base_url().'images/menubar/'.$module->module_id.'.png';?>" class="circle" />
+				<br />				
+				<?php echo $this->lang->line("module_".$module->module_id) ?>
+			</a>
+			
+			- <?php echo $this->lang->line('module_'.$module->module_id.'_desc');?>
+		</div>
 	</div>
 	<?php
 		}
